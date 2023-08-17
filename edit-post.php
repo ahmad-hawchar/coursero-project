69a889128f22159eb030bbb9d57c2e4ed31e14d7
ahmad-hawchar <hawchar.business@gmail.com>
2023-08-18 01:14:20 +0300

    completed the edit post pages

--- edit-post.php
+++ edit-post.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-if (!$_SESSION['isloggedin'] || $_SESSION['role'] != 1 || !isset($_GET['post'])) {
-    header("Location:login.php");
-    return;
-}
+//if (!$_SESSION['isloggedin'] || $_SESSION['role'] != 1 || !isset($_GET['post'])) {
+  //  header("Location:login.php");
+    //return;
+//}
 require_once('connection.php')
     ?>
 <!DOCTYPE html>
@@ -73,27 +73,32 @@ else{
       </nav>
     </header>
     <!-- Script -->
-    <script src="script.js"></script>
+    <script src="script.js"> </script>
     <div style="background-color:#e9e9e9; margin-bottom:10px">
         <center>
           <h2 >Edit post</h2>
         </center>
       </div>
     <?php
-    $post = $_GET['post'];
-    $id = $_SESSION["id"];
-    $query = "SELECT * from post where Id=$post and Id_t=$id";
+    if(isset($_GET['post'])){
+      $post=$_GET['post'];
+    }
+    else{
+      header('Location:homepage.php');
+    }
+    $id =$_SESSION["id"];
+    $query ="SELECT * from post where Id=$post and Id_t=$id";
     $res = mysqli_query($con, $query);
-    if (mysqli_num_rows($res) < 0) {
+    if (mysqli_num_rows($res)==0) {
         echo "you don't have access to this post";
 
     } else {
         $row1 = mysqli_fetch_assoc($res);
         echo "<h1>edit your Post</h1>";
-        echo "<form action='edit-post2.php' enctype='file' method='GET'>";
-        echo " <label for='photo'>Photo:</label>";
-        echo " <input type='file' id='photo' name='photo' required>";
-        echo " <label for='description'>Description:</label>";
+        echo "<form action='edit-post2.php' method='POST' enctype='multipart/form-data'>";
+        echo "<label for='photo'>Photo:</label>";
+        echo " <input type='file'name='photo' required accept='image/*'>";
+        echo "<label for='description'>Description:</label>";
         echo "<textarea id='description' name='description' rows='4' cols='50'  required></textarea>";
         echo"<script> let doc=document.getElementById('description')
         doc.innerText='$row1[Description]'</script>";
@@ -131,10 +136,10 @@ else{
             echo "<td style='color:red'>we had a problem creating your post! try again later</td>";
         }
         if (isset($_GET['done'])) {
-            echo "<td style='color:red'>post was created! waiting for admin approval.</td>";
+            echo "<td style='color:red'>post edited successfully! .</td>";
         }
-        echo"<input type='text' id='temp' style='visibility:hidden' /> ";
-        echo "<script> let temp=document.getElementById('temp');
-        temp.value='$post' </script>";
-        echo "</table></form></body></html>";
+        echo"<input type='text' id='postid' name='postid' hidden='true' /> ";
+        echo"<script> let postid=document.getElementById('postid');
+        postid.value='$post' </script>";
+        echo"</table></form></body></html>";
     } ?>
--- edit-post2.php
+++ edit-post2.php
@@ -1,39 +1,39 @@
 <?php
 session_start();
-//if(!isset($_SESSION['isloggedin'])){
- //header("Location:login.php");
-  //  return;
-//}
-//if ($_SESSION['isloggedin'] !="true" || $_SESSION['role'] != 1) {
-   // header("Location:login.php");
-   // return;
-//}
+?>
+<?php
+if(!isset($_SESSION['isloggedin'])){
+ header("Location:login.php");
+    return;
+}
+if ($_SESSION['isloggedin'] !="true" || $_SESSION['role'] != 1) {
+    header("Location:login.php");
+    return;
+}
 require_once('connection.php');
 if (
-    (isset($_FILES['photo']) && $_FILES['photo'] != "") &&
-    (isset($_GET['description']) && $_GET['description'] != "") &&
-    (isset($_GET['cat']) && $_GET['cat'] != "") &&
-    (isset($_GET['price']) && $_GET['price'] != "")&&
-    (isset($_GET['temp']) && $_get['temp']!="")
+   //(isset($_FILES['photo']) && $_FILES['photo'] != "")&&
+   (isset($_POST['description']) && $_POST['description'] != "") &&
+    (isset($_POST['cat']) && $_POST['cat'] != "") &&
+    (isset($_POST['price']) && $_POST['price'] != "")&&
+   (isset($_POST['postid']) && $_POST['postid']!="")
 ) {
-    if(isset($_FILES["photo"])){
-        $photo=$_FILES["photo"]["name"];
-        move_uploaded_file($_FILES["photo"]["tmp_name"],"photos/$photo");
-    }
-    echo"test";
-    $desc = $_GET['description'];
-    $cat = $_GET['cat'];
+    $desc = $_POST['description'];
+    $cat = $_POST['cat'];
     $date = date("Y-m-d");
     $id = $_SESSION['id'];
-    $price = $_GET['price'];
-    $p_id=$_GET['Id'];
-    $post=$_GET['temp'];
-    $query = "UPDATE 'post' SET values('$p_id','$date','$desc','$id','$cat','$photo','$price' ) where  Id=$post and Id_t=$id";
+    $price = $_POST['price'];
+    $photo = $_FILES['photo']['name'];
+    $photo_tmp = $_FILES['photo']['tmp_name'];
+    move_uploaded_file($photo_tmp, "photos/$photo");
+    $p_id=$_POST['postid'];
+    $query ="UPDATE `post` SET `Id`='$p_id',`Date`='$date',`Description`='$desc',`Id_t`='$id',`category`='$cat',`picture`='$Photo',`price`='$price'  where  Id=$p_id and Id_t=$id ;";
+    echo"$query";
     $result = mysqli_query($con, $query);
-    $result ? header("Location:edit-post.php?done=''") : header("Location:edit-post.php?error=''");
+    $result ? header("Location:edit-post.php?done=''&post='$p_id'") : header("Location:edit-post.php?error=''&post='$p_id'");
 } else {
-    header("Location:edit-post.php?error=''");
-
+    $p_id=$_POST['postid'];
+    header("Location:edit-post.php?error=''&post=$p_id");
 }
 
 
